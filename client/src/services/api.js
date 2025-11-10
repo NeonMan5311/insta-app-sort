80e74ff8d6a2225de31ba78b149d3cdcfd3fdd25
NeonMan5311 <dhengleyadav@gmail.com>
2025-11-10 12:08:19 +0530

    Update API URL to include '/api' path

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = "https://insta-app-sort.onrender.com";
+const API_URL = "https://insta-app-sort.onrender.com/api";
 
 export const createJob = (profile_url, user_email) => {
 	return axios.post(`${API_URL}/jobs`, { profile_url, user_email });
@@ -10,4 +10,5 @@ export const createJob = (profile_url, user_email) => {
 export const getJob = (result_id) => {
 	return axios.get(`${API_URL}/jobs/${result_id}`);
 };
+
 
