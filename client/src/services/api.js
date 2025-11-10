e0150e26cf657d668221a6744da3c75d7dae4498
NeonMan5311 <dhengleyadav@gmail.com>
2025-11-10 12:04:05 +0530

    Update API_URL to point to production server

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 
 import axios from "axios";
 
-const API_URL = "/api";
+const API_URL = "https://insta-app-sort.onrender.com";
 
 export const createJob = (profile_url, user_email) => {
 	return axios.post(`${API_URL}/jobs`, { profile_url, user_email });
@@ -10,3 +10,4 @@ export const createJob = (profile_url, user_email) => {
 export const getJob = (result_id) => {
 	return axios.get(`${API_URL}/jobs/${result_id}`);
 };
+
